4fa84bc430fb20202c1cb36f526d7b5de1672f14
Federico Marra <marrafederico@proton.me>
2026-03-27 10:44:46 +0100

    update styles dashboard

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,11 @@ import { FileSystemService } from '../../services/file-system.service';
 
 // Material component
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatDividerModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
@@ -24,5 +22,9 @@ export class NavbarComponent {
     });
 
     this.fileSystemService.rootDirectory.set(rootHandle);
+  }
+
+  async navigateToRoot() {
+    await this.fileSystemService.navigateToRoot();
   }
 }
